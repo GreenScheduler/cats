7d275338a32fda40807a4a643b4839af7d29a166
Abhishek Dasgupta <abhishek.dasgupta@dtc.ox.ac.uk>
2023-05-04 13:37:31 +0100

    Run black on tests

--- tests/test_timeseries_conversion.py
+++ tests/test_timeseries_conversion.py
@@ -6,9 +6,14 @@ import cats
 
 TEST_DATA = Path(__file__).parent / "carbon_intensity_24h.csv"
 
+
 def test_stub():
     pass
 
+
 def test_timeseries_conversion():
     result = cats.cat_converter(TEST_DATA)
-    assert result == {'timestamp': datetime.datetime(2023, 5, 4, 12, 30), 'carbon_intensity': 2.0}
+    assert result == {
+        "timestamp": datetime.datetime(2023, 5, 4, 12, 30),
+        "carbon_intensity": 2.0,
+    }
