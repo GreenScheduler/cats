8d8455652c5bb37385891e2c18cf6d2a8ae6d275
Thibault Lestang <t.lestang@imperial.ac.uk>
2023-05-04 12:43:21 +0100

    Initial draft for the at wrapper

---
+++ cats.py
@@ -0,0 +1,15 @@
+from argparse import ArgumentParser
+import requests
+from cats import findtime
+
+parser = ArgumentParser(prog="cats", description="A climate ware job scheduler")
+
+parser.add_argument("program")
+parser.add_argument("--loc")
+parser.add_argument("-d", "--duration", required=True)
+
+args = parser.parse_args()
+
+if not args.loc:
+    r = requests.get("https://ipapi.co/json").json()
+    loc = r["postal"]
