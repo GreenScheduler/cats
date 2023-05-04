285642c823eb86cd22bf42d256cb4e460b6571d0
Lincoln Colling <lincoln@colling.net.nz>
2023-05-04 14:26:50 +0100

    fixed tests

--- tests/test_timeseries_conversion.py
+++ tests/test_timeseries_conversion.py
@@ -14,6 +14,7 @@ def test_stub():
 def test_timeseries_conversion():
     result = cats.cat_converter(TEST_DATA)
     assert result == {
-        "timestamp": datetime.datetime(2023, 5, 4, 12, 30),
-        "carbon_intensity": 2.0,
+        "timestamp": datetime.datetime(2023, 5, 5, 14, 0),
+        "carbon_intensity": 5.0,
+        "est_total_carbon": 5.0
     }
