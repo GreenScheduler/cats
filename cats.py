45f9d843c77b2277006f241333081bd35ab5071b
Thibault Lestang <t.lestang@imperial.ac.uk>
2023-05-04 14:17:01 +0100

    Define main() entry point
    Co-authored-by: tonymugen <info@bayesicresearch.org>

--- cats.py
+++ cats.py
@@ -2,27 +2,39 @@ from argparse import ArgumentParser
 import requests
 import subprocess
 
-from cats import findtime
+# from cats import findtime
 
-parser = ArgumentParser(prog="cats", description="A climate ware job scheduler")
 
-parser.add_argument("program")
-parser.add_argument("--loc")
-parser.add_argument("-d", "--duration", required=True)
+def parse_arguments():
+    parser = ArgumentParser(prog="cats", description="A climate ware job scheduler")
 
-args = parser.parse_args()
+    parser.add_argument("program")
+    parser.add_argument("--loc")
+    parser.add_argument("-d", "--duration", required=True)
 
-if not args.loc:
-    r = requests.get("https://ipapi.co/json").json()
-    loc = r["postal"]
-runtime = findtime()
+    return parser
 
-subprocess.run(
-    [
-        args.program,
-        "|",
-        "at",
-        "-m",
-        f"{runtime:%m%d%H%M}",
-    ]
-)
+
+def main(arguments=None):
+    parser = parse_arguments()
+    args = parser.parse_args(arguments)
+
+    if not args.loc:
+        r = requests.get("https://ipapi.co/json").json()
+        loc = r["postal"]
+
+        runtime = findtime()
+
+        subprocess.run(
+            [
+                args.program,
+                "|",
+                "at",
+                "-m",
+                f"{runtime:%m%d%H%M}",
+            ]
+        )
+
+
+if __name__ == "__main__":
+    main()
