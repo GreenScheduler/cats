77f5611fdd0d9a0e99e70b037aedede0f67437a6
Thibault Lestang <t.lestang@imperial.ac.uk>
2023-05-05 15:24:29 +0100

    Add a --jobinfo cli option
    Providing this option enables the estimation of carbon footprint.

--- cats/__init__.py
+++ cats/__init__.py
@@ -2,6 +2,7 @@ from argparse import ArgumentParser
 from datetime import timedelta
 import requests
 import subprocess
+import re
 import yaml
 import sys
 
@@ -26,10 +27,53 @@ def parse_arguments():
     #parser.add_argument("program")
     parser.add_argument("--loc")
     parser.add_argument("-d", "--duration", required=True)
+    parser.add_argument("--jobinfo")
 
     return parser
 
 
+def validate_jobinfo(jobinfo: str):
+    """Parses a string of job info keys in the form
+
+    partition=CPU_partition,memory=8,ncpus=8,ngpus=0
+
+    and checks all required info keys are present and of the right type.
+
+    Returns
+    -------
+
+    info: dict
+        A dictionary mapping info key to their specified values
+    """
+
+    expected_info_keys = (
+        "partition",
+        "memory",
+        "cpus",
+        "gpus",
+    )
+    info = {
+        k: v
+        for k, v in [match.groups() for match in re.finditer(r"(\w+)=(\w+)", jobinfo)]
+    }
+    missing_keys = set(expected_info_keys) - set(info.keys())
+    if missing_keys:
+        print(f"ERROR: Missing job info keys: {missing_keys}")
+        return {}
+    if info["partition"] not in ("CPU_partition", "GPU_partition"):
+        msg = (
+            "ERROR: job info key 'partition' should be "
+            f"one of {expected_partition_values}. Typo?"
+        )
+        print(msg)
+        return {}
+    for key in [k for k in info.keys() if k != "partition"]:
+        try:
+            info[key] = int(info[key])
+        except ValueError:
+            print(f"ERROR: job info key {key} should be numeric")
+            return {}
+    return info
 def main(arguments=None):
     with open("config.yml", "r") as f:
         config = yaml.safe_load(f)
@@ -58,16 +102,19 @@ def main(arguments=None):
 #        ]
 #    )
 
-    footprint = greenAlgorithmsCalculator(
-        partition = args.partition,
-        runtime = timedelta(minutes = 120),
-        memory = args.memory,
-        nCPUcores = args.ncpus,
-        nGPUcores = args.ngpus,
-        averageBest_carbonIntensity = 80.,
-        averageNow_carbonIntensity = 162.,
-        starttime=runtime
-    ).get_footprint()
+
+    if args.jobinfo:
+        jobinfo = validate_jobinfo(args.jobinfo)
+        if not jobinfo:
+            print("ERROR: job info parsing failed, exiting now")
+            exit(1)
+        estim = greenAlgorithmsCalculator(
+            runtime=timedelta(minutes=args.duration),
+            starttime=runtime,
+            averageBest_carbonIntensity=80,
+            averageNow_carbonIntensity=290,
+            **jobinfo,
+        ).get_footprint()
 
 
 if __name__ == "__main__":
