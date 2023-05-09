80dd4d8a581aeec19cdcdbdc24af960a7bfec1cd
Thibault Lestang <t.lestang@imperial.ac.uk>
2023-05-09 09:17:56 +0100

    fix: configgg -> config. Exit on config file not found.

--- cats/__init__.py
+++ cats/__init__.py
@@ -82,16 +82,13 @@ def main(arguments=None):
     parser = parse_arguments()
     args = parser.parse_args(arguments)
 
-    if args.config:
-        with open(args.config, "r") as f:
+    config_path = args.config if args.config else "config.yml"
+    try:
+        with open(config_path, "r") as f:
             config = yaml.safe_load(f)
-    else:
-        # if no path provided, look for `config.yml` in current directory
-        try:
-            with open("configgg.yml", "r") as f:
-                config = yaml.safe_load(f)
-        except FileNotFoundError:
-            config = dict()
+    except FileNotFoundError:
+        print("ERROR: Could not find config file {config_path}")
+        exit(1)
 
     if not args.loc:
         if "postcode" not in config.keys():
