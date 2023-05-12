c36325bf8b233d6389b8f63ef169cfa01674ff7f
Thibault Lestang <thibault.lestang@protonmail.com>
2023-05-12 21:30:19 +0100

    Add first draft of windowed forecast class

---
+++ cats/forecast.py
@@ -0,0 +1,29 @@
+from datetime import datetime, timedelta
+
+
+class WindowedForecast:
+
+    def __init__(self, data: list[tuple[datetime, int]], window_size: int):
+        self.times = [row[0] for row in data]
+        self.intensities = [row[1] for row in data]
+        self.window_size = window_size
+        self.size = len(self.times) - window_size
+
+    def __getitem__(self, index):
+        avg = sum(
+            self.intensities[index:index + self.window_size]
+        ) / self.window_size
+        return (self.times[index], avg)
+
+    def __iter__(self):
+        for index in range(self.size + 1):
+            yield self.__getitem__(index)
+
+import math
+data = [
+    (datetime.today() + timedelta(minutes=i), math.sin(i * (math.pi / 10)))
+    for i in range(10)
+]
+        
+        
+    
