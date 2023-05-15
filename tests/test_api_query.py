cdb1fe2058bae7eb9b1f60626fea3cafa2f2a44d
Thibault Lestang <t.lestang@imperial.ac.uk>
2023-05-15 11:21:27 +0100

    tests: Update get_tuple interface

--- tests/test_api_query.py
+++ tests/test_api_query.py
@@ -1,11 +1,17 @@
 import cats
+from cats.api_interface import API_interfaces
 
 def test_api_call():
     """
     This just checks the API call runs and returns a list of tuples
     """
 
-    response = cats.api_query.get_tuple('OX1')
+    api_interface = API_interfaces["carbonintensity.org.uk"]
+    response = cats.api_query.get_tuple(
+        postcode='OX1',
+        request_url=api_interface.get_request_url,
+        parse_data_from_json=api_interface.parse_response_data,
+    )
 
     assert isinstance(response, list) 
     for item in response:
