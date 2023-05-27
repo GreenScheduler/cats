bcc5d64aa26e3670da69d63b7c7726d9a24d6c55
Loïc Lannelongue <loic.lannelongue@posteo.net>
2023-05-27 15:22:54 +0100

    Create cats class with __init__ for args, config file and location

--- cats/__init__.py
+++ cats/__init__.py
@@ -1,160 +1,51 @@
 from argparse import ArgumentParser
-from datetime import datetime, timedelta
 import requests
-import subprocess
-import re
 import yaml
-import sys
 
-from .timeseries_conversion import cat_converter  # noqa: F401
-from .api_query import get_tuple  # noqa: F401
-from .parsedata import writecsv, avg_carbon_intensity  # noqa: F401
-from .api_interface import API_interfaces
-from .carbonFootprint import greenAlgorithmsCalculator
+class cats():
+    def __init__(self, arguments=None):
+        parser = self.parse_arguments()
+        args = parser.parse_args(arguments)
 
-# from cats import findtime
+        ### config file ###
 
-
-def findtime(postcode, duration, api_interface):
-    tuples = get_tuple(
-        postcode,
-        api_interface.get_request_url,
-        api_interface.parse_reponse_data,
-    )
-    result = writecsv(tuples, duration)
-    sys.stderr.write(str(result) + "\n")
-    return result
-
-
-def parse_arguments():
-    parser = ArgumentParser(prog="cats", description="A climate aware job scheduler")
-
-    #parser.add_argument("program")
-    parser.add_argument("--loc")
-    parser.add_argument("-d", "--duration", type=int, required=True)
-    parser.add_argument("--jobinfo")
-    parser.add_argument("--config")
-
-    return parser
-
-
-def validate_jobinfo(jobinfo: str):
-    """Parses a string of job info keys in the form
-
-    partition=CPU_partition,memory=8,ncpus=8,ngpus=0
-
-    and checks all required info keys are present and of the right type.
-
-    Returns
-    -------
-
-    info: dict
-        A dictionary mapping info key to their specified values
-    """
-
-    expected_info_keys = (
-        "partition",
-        "memory",
-        "cpus",
-        "gpus",
-    )
-    info = {
-        k: v
-        for k, v in [match.groups() for match in re.finditer(r"(\w+)=(\w+)", jobinfo)]
-    }
-    missing_keys = set(expected_info_keys) - set(info.keys())
-    if missing_keys:
-        print(f"ERROR: Missing job info keys: {missing_keys}")
-        return {}
-    expected_partition_values = ("CPU_partition", "GPU_partition")
-    if info["partition"] not in expected_partition_values:
-        msg = (
-            "ERROR: job info key 'partition' should be "
-            f"one of {expected_partition_values}. Typo?"
-        )
-        print(msg)
-        return {}
-    for key in [k for k in info.keys() if k != "partition"]:
-        try:
-            info[key] = int(info[key])
-        except ValueError:
-            print(f"ERROR: job info key {key} should be numeric")
-            return {}
-    return info
-
-
-def main(arguments=None):
-    parser = parse_arguments()
-    args = parser.parse_args(arguments)
-
-    if args.config:
-        with open(args.config, "r") as f:
-            config = yaml.safe_load(f)
-    else:
-        # if no path provided, look for `config.yml` in current directory
-        try:
-            with open("config.yml", "r") as f:
-                config = yaml.safe_load(f)
-        except FileNotFoundError:
-            config = dict()
-
-    if not args.loc:
-        if "postcode" not in config.keys():
-            r = requests.get("https://ipapi.co/json").json()
-            loc = r["postal"]
+        if args.config:
+            # if path to config file provided, it is used
+            with open(args.config, "r") as f:
+                self.config = yaml.safe_load(f)
         else:
-            loc = config["postcode"]
-    else:
-        loc = args.loc
-    #print("Location:", loc)
+            try:
+                # if no path provided, look for `config.yml` in current directory
+                with open("config.yml", "r") as f:
+                    self.config = yaml.safe_load(f)
+            except FileNotFoundError:
+                # config file not essential, so initialised as empty dict if missing
+                self.config = {}
 
-    best_estimate = findtime(
-        loc, args.duration,
-        # TODO Choose API provider based on postcode or
-        # user option
-        API_interfaces["carbonintensitity.org.uk"],
-    )
-#    subprocess.run(
-#        [
-#            args.program,
-#            "|",
-#            "at",
-#            "-m",
-#            f"{runtime:%m%d%H%M}",
-#        ]
-#    )
+        ### Pull and clean postcode ###
+        # UK: we only keep the first part of a UK postcode
 
-    sys.stderr.write(f"Best job start time: {best_estimate.start}\n")
-    print(f"{best_estimate.start:%Y%m%d%H%M}")  # for POSIX compatibility with at -t
+        if args.loc:
+            self.loc = args.loc
+        elif "postcode" in self.config.keys():
+            self.loc = self.config["postcode"]
+        else:
+            self.loc = self.pull_location_from_IP()
 
-    if args.jobinfo:
-        jobinfo = validate_jobinfo(args.jobinfo)
-        if not jobinfo:
-            print("ERROR: job info parsing failed, exiting now")
-            exit(1)
-        if not config:
-            print("ERROR: config file not found, exiting now")
-            exit(1)
-        now_avg_ci = avg_carbon_intensity(
-            start=datetime.now(), runtime=timedelta(args.duration)
-        )
-        estim = greenAlgorithmsCalculator(
-            config=config,
-            runtime=timedelta(minutes=args.duration),
-            averageBest_carbonIntensity=best_estimate.value, # TODO replace with real carbon intensity
-            averageNow_carbonIntensity=now_avg_ci,
-            **jobinfo,
-        ).get_footprint()
 
-        print(" -!-!- Carbon footprint estimation is a work in progress, coming soon!")
-        # Commenting these out while waiting for real carbon intensities
-        # print(f"Estimated emmissions for running job now: {estim.now}")
-        # msg = (
-        #     f"Estimated emmissions for running delayed job: {estim.best})"
-        #     f" (- {estim.savings})"
-        # )
-        # print(msg)
+    def parse_arguments(self):
+        parser = ArgumentParser(prog="cats", description="A climate aware job scheduler")
+        # parser.add_argument("program")
+        parser.add_argument("--loc")
+        parser.add_argument("-d", "--duration", type=int, required=True)
+        parser.add_argument("--jobinfo")
+        parser.add_argument("--config")
 
+        return parser
+
+    def pull_location_from_IP(self):
+        r = requests.get("https://ipapi.co/json").json()
+        return r["postal"]
 
 if __name__ == "__main__":
-    main()
+    cats()
