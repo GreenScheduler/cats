c458ca2f15c8f1061b863f22a9156b5a60fe838b
Loïc Lannelongue <loic.lannelongue@posteo.net>
2023-05-28 14:16:08 +0100

    improved clarity

--- cats/optimise_starttime.py
+++ cats/optimise_starttime.py
@@ -8,26 +8,41 @@ class starttime_optimiser():
         self.CI_forecast = CI_forecast
 
     def _calculate_averageCI_over_runtime(self, starttime, endtime):
+        '''
+        Calculate the total average CI between `starttime` and `endtime`
+        by integrating over each time window (and taking into account the
+        fact that first and last time windows are probably incomplete)
+        :param starttime: [datetime]
+        :param endtime: [datetime]
+        :return: [float] average CI in CO2e
+        '''
         # TODO deal with runtime running past end of forecast
 
         # Only keep the relevant windows in the total forecast
         filtered_forecast = [
             window for window in self.CI_forecast
-            if window.end > starttime and window.start < endtime
+            if starttime < window.end and window.start < endtime
         ]
 
         # Calculate average CI between starttime and endtime
-        CIsec = 0
-        totalsec = 0
+        CIsec = 0 # in gCO2e x seconds
+        totalsec = 0 # in seconds
         for window in filtered_forecast:
-            # find the true duration of the window [timedelta] (as first and last won't be a full 30min, or whatever the window size is)
+            # find the true duration of the window [timedelta]
+            # (this is needed because first and last windows won't be a full 30min, or whatever the window size is)
             true_window = min(endtime, window.end) - max(starttime, window.start)
             CIsec += window.value * true_window.total_seconds()
             totalsec += true_window.total_seconds()
+
         return CIsec/totalsec
 
-
     def _get_all_averageCIs(self, duration):
+        '''
+        Calculate all the average CIs for all possible start times.
+        The first possible time is now, the last possible time is "End of forecast - job duration".
+        :param duration: [timedelta]
+        :return: [list[CarbonIntensityEstimate]] All possible average CIs
+        '''
         currenttime = datetime.now(timezone.utc)
         end_of_forecast = self.CI_forecast[-1].end
 
@@ -35,12 +50,13 @@ class starttime_optimiser():
 
         # What if we start the job now
         endtime = currenttime + duration
-        if endtime <= end_of_forecast:
-            all_averageCIs.append(CarbonIntensityEstimate(
-                start=currenttime,
-                end=endtime,
-                value=self._calculate_averageCI_over_runtime(currenttime, endtime),
-            ))
+        assert endtime <= end_of_forecast, "Job is longer than total forecasted carbon intensities"
+
+        all_averageCIs.append(CarbonIntensityEstimate(
+            start=currenttime,
+            end=endtime,
+            value=self._calculate_averageCI_over_runtime(currenttime, endtime),
+        ))
 
         # Go through all the possible starttimes and calculate average CIs
         for window in self.CI_forecast:
@@ -58,6 +74,11 @@ class starttime_optimiser():
         return all_averageCIs
 
     def get_starttime(self, duration):
+        '''
+        Find best possible starttime to minimise carbon intensities
+        :param duration: [timedelta]
+        :return: [tuple(CarbonIntensityEstimate, list[CarbonIntensityEstimate])]
+        '''
         all_averageCIs = self._get_all_averageCIs(duration)
         # Find the minimum value
         all_averageCIs_sorted = sorted(all_averageCIs)
