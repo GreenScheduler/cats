dce793adab2072f8c96add3f3aac1904600a9302
Loïc Lannelongue <loic.lannelongue@posteo.net>
2023-05-29 23:42:28 +0100

    updated CI_API tests

--- tests/test_api_query.py
+++ tests/test_api_query.py
@@ -1,18 +1,15 @@
 import cats
-from cats.CI_api_interface import API_interfaces
+from cats.CI_api_query import CI_API
+from cats.CI_api_interface import CarbonIntensityEstimate
 
 def test_api_call():
     """
     This just checks the API call runs and returns a list of tuples
     """
 
-    api_interface = API_interfaces["carbonintensity.org.uk"]
-    response = cats.api_query.get_tuple(
-        postcode='OX1',
-        request_url=api_interface.get_request_url,
-        parse_data_from_json=api_interface.parse_response_data,
-    )
+    APIcall = CI_API(choice_CI_API='carbonintensity.org.uk')
+    parsed_data = APIcall.get_forecast("M15")
 
-    assert isinstance(response, list) 
-    for item in response:
-        assert isinstance(item, tuple) 
+    assert isinstance(parsed_data, list)
+    for item in parsed_data:
+        assert isinstance(item, CarbonIntensityEstimate)
