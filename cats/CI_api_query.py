83c46245ded7672125392f32fb45ef951697ff6c
Loïc Lannelongue <loic.lannelongue@posteo.net>
2023-07-12 21:08:33 +0100

    Move datetime 30min standardisation to the API interface

--- cats/CI_api_query.py
+++ cats/CI_api_query.py
@@ -13,24 +13,13 @@ def get_CI_forecast(location: str, CI_API_interface) -> list[CarbonIntensityPoin
     param location: [str] Depends on country. UK postcode (just the first section), e.g. M15.
     returns: a list of CarbonIntensityPointEstimate
     """
-    # get the time (as a datetime object) and update this to be the 'top' of
-    # the current hour or half hour in UTZ plus one minute. So a call at 
-    # 17:47 BST will yield a timestamp of 16:31 UTC. This means that within
-    # any given half hour we will always use the same timestamp. As this 
-    # becomes part of the URL, calls can be cached using standard HTTP 
-    # caching layers
-    dt = datetime.now(timezone.utc)
-    if dt.minute > 30:
-        dt = dt.replace(minute=31, second=0, microsecond=0)
-    else:
-        dt = dt.replace(minute=1, second=0, microsecond=0)
 
     # Setup a session for the API call. This uses a global HTTP cache
     # with the URL as the key. Failed attempts are not cached.
     session = requests_cache.CachedSession('cats_cache', use_temp=True)
 
     # get the carbon intensity api data
-    r = session.get(CI_API_interface.get_request_url(dt, location))
+    r = session.get(CI_API_interface.get_request_url(datetime.now(timezone.utc), location))
     data = r.json()
 
     return CI_API_interface.parse_response_data(data)
