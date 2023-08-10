b583e715786e4dc6ea42273300678e0007452e9d
Andrew Walker <andrew.walker@earth.ox.ac.uk>
2023-08-10 16:41:41 +0100

    Make sure our timestamps are timezone aware
    This probably needs to be done on an api-interface
    by api-interface basis (as how we do it depends on
    how we convert whatever comes back into a datetime
    object). For the ciuk we have explicit UTC returned
    but strptime does not know how to parse this. So
    add the timezone info for each datetime object.
    
    Also modify the test to make sure all datetime
    objects we get back from the api_query are aware
    of their datetime (we don't care what this is,
    only that it is defined). We can then convert to
    system time at the other end.

--- tests/test_api_query.py
+++ tests/test_api_query.py
@@ -6,7 +6,8 @@ from cats.forecast import CarbonIntensityPointEstimate
 
 def test_api_call():
     """
-    This just checks the API call runs and returns a list of tuples
+    This just checks the API call runs and returns a list of point estimates
+    and confirms that datetime objects are timezone aware
     """
 
     api_interface = API_interfaces["carbonintensity.org.uk"]
@@ -15,6 +16,8 @@ def test_api_call():
     assert isinstance(response, list)
     for item in response:
         assert isinstance(item, CarbonIntensityPointEstimate)
+        assert ((item.datetime.tzinfo is not None) and
+                (item.datetime.tzinfo.utcoffset(item.datetime) is not None))
 
 def test_bad_postcode():
     api_interface = API_interfaces["carbonintensity.org.uk"]
@@ -24,3 +27,4 @@ def test_bad_postcode():
 
     with pytest.raises(InvalidLocationError):
         response = cats.CI_api_query.get_CI_forecast('A', api_interface)
+
