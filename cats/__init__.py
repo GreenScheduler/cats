8c048f64ccb3fbec57033fb86b7c38695808377c
Abhishek Dasgupta <abhishek.dasgupta@dtc.ox.ac.uk>
2024-02-11 21:48:01 +0000

    Refactor output logic into dataclass
    Adds --format option for output formatting, currently
    only JSON is supported
    
    Change sys.stderr.write to logging calls
    
    Fixes: #67

--- cats/__init__.py
+++ cats/__init__.py
@@ -1,14 +1,20 @@
 from argparse import ArgumentParser
+from typing import Optional
 from datetime import datetime, timedelta
+import dataclasses
 import requests
+import logging
 import yaml
 import sys
+from pprint import pprint
+import json
 
 from .check_clean_arguments import validate_jobinfo, validate_duration
 from .optimise_starttime import get_avg_estimates  # noqa: F401
 from .CI_api_interface import API_interfaces, InvalidLocationError
 from .CI_api_query import get_CI_forecast  # noqa: F401
-from .carbonFootprint import greenAlgorithmsCalculator
+from .carbonFootprint import greenAlgorithmsCalculator, Estimates
+from .forecast import CarbonIntensityAverageEstimate
 
 def parse_arguments():
     """
@@ -93,7 +99,38 @@ def parse_arguments():
              "Default: if absent, the total carbon footprint is not estimated."
     )
 
+    parser.add_argument("--format", type=str, help="Format to output optimal start time and carbon emmission"
+                        "estimate savings in. Currently only JSON is supported", choices=["json"])
+
     return parser
+
+@dataclasses.dataclass
+class CATSOutput:
+    """Carbon Aware Task Scheduler output"""
+
+    carbonIntensityAPI: str
+    carbonIntensityNow: CarbonIntensityAverageEstimate
+    carbonIntensityOptimal: CarbonIntensityAverageEstimate
+    location: str
+    countryISO3: str
+    emmissionEstimate: Optional[Estimates] = None
+
+    def __str__(self) -> str:
+        out = f"Best job start time: {self.carbonIntensityOptimal.start}\n"
+
+        if self.emmissionEstimate:
+            out += (f"Estimated emmissions for running job now: {self.emmissionEstimate.now}\n"
+                f"Estimated emmissions for running delayed job: {self.emmissionEstimate.best})\n"
+                f" (- {self.emmissionEstimate.savings})"
+            )
+        return out
+
+    def to_json(self, **kwargs) -> str:
+        data = dataclasses.asdict(self)
+        for ci in ["carbonIntensityNow", "carbonIntensityOptimal"]:
+            data[ci]["start"] = data[ci]["start"].isoformat()
+            data[ci]["end"] = data[ci]["end"].isoformat()
+        return json.dumps(data, **kwargs)
 
 
 def main(arguments=None):
@@ -109,16 +146,16 @@ def main(arguments=None):
         # if path to config file provided, it is used
         with open(args.config, "r") as f:
             config = yaml.safe_load(f)
-        sys.stderr.write(f"Using provided config file: {args.config}\n")
+        logging.info(f"Using provided config file: {args.config}\n")
     else:
         # if no path provided, look for `config.yml` in current directory
         try:
             with open("config.yml", "r") as f:
                 config = yaml.safe_load(f)
-            sys.stderr.write("Using config.yml found in current directory\n")
+            logging.info("Using config.yml found in current directory\n")
         except FileNotFoundError:
             config = {}
-            sys.stderr.write("WARNING: config file not found\n")
+            logging.warning("config file not found")
 
     ## CI API choice
     list_CI_APIs = ['carbonintensity.org.uk']
@@ -131,20 +168,20 @@ def main(arguments=None):
 
     if choice_CI_API not in list_CI_APIs:
         raise ValueError(f"{choice_CI_API} is not a valid API choice, it needs to be one of {list_CI_APIs}.")
-    sys.stderr.write(f"Using {choice_CI_API} for carbon intensity forecasts\n")
+    logging.info(f"Using {choice_CI_API} for carbon intensity forecasts\n")
 
     ## Location
     if args.location:
         location = args.location
-        sys.stderr.write(f"Using location provided: {location}\n")
+        logging.info(f"Using location provided: {location}")
     elif "location" in config.keys():
         location = config["location"]
-        sys.stderr.write(f"Using location from config file: {location}\n")
+        logging.info(f"Using location from config file: {location}")
     else:
         r = requests.get("https://ipapi.co/json").json()
         postcode = r["postal"]
         location = postcode
-        sys.stderr.write(f"WARNING: location not provided. Estimating location from IP address: {location}.\n")
+        logging.warning(f"location not provided. Estimating location from IP address: {location}.")
 
     ## Duration
     duration = validate_duration(args.duration)
@@ -157,8 +194,8 @@ def main(arguments=None):
     try:
         CI_forecast = get_CI_forecast(location, CI_API_interface)
     except InvalidLocationError:
-        sys.stderr.write(f"Error: unknown location {location}\n")
-        sys.stderr.write(
+        logging.error(f"Error: unknown location {location}\n")
+        logging.error(
             "Location should be be specified as the outward code,\n"
             "for example 'SW7' for postcode 'SW7 EAZ'.\n"
         )
@@ -173,38 +210,30 @@ def main(arguments=None):
     now_avg, best_avg = get_avg_estimates(
         CI_forecast, duration=duration
     )
-    sys.stderr.write(str(best_avg) + "\n")
-
-    sys.stderr.write(f"Best job start time: {best_avg.start}\n")
-    print(f"{best_avg.start:%Y%m%d%H%M}")  # for POSIX compatibility with at -t
+    output = CATSOutput(choice_CI_API, now_avg, best_avg, location, "GBR")
 
     ################################
     ## Calculate carbon footprint ##
     ################################
 
-    error_message = "Not enough information to estimate total carbon footprint, both --jobinfo and config files are needed.\n"
-
     if args.jobinfo:
         jobinfo = validate_jobinfo(args.jobinfo, expected_partition_names=config['partitions'].keys())
 
         if not (jobinfo and config):
-            sys.stderr.write(error_message)
+            logging.warning("Not enough information to estimate total carbon footprint, "
+                            "both --jobinfo and config files are needed.\n")
         else:
-            estim = greenAlgorithmsCalculator(
+            output.emmissionEstimate = greenAlgorithmsCalculator(
                 config=config,
                 runtime=timedelta(minutes=args.duration),
                 averageBest_carbonIntensity=best_avg.value, # TODO replace with real carbon intensity
                 averageNow_carbonIntensity=now_avg.value,
                 **jobinfo,
             ).get_footprint()
-
-            sys.stderr.write(f"Estimated emmissions for running job now: {estim.now}\n")
-            msg = (
-                f"Estimated emmissions for running delayed job: {estim.best})\n"
-                f" (- {estim.savings})"
-            )
-            sys.stderr.write(msg)
-
+    if args.format == "json":
+        print(output.to_json(sort_keys=True, indent=2))
+    else:
+        print(output)
 
 if __name__ == "__main__":
     main()
