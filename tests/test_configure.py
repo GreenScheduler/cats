d1c52835cb86162cb83e50a77df300e0885ceec1
Thibault Lestang <thibault.lestang@meteo.fr>
2024-03-07 18:32:11 +0100

    Add tests for configuring config file and location

---
+++ tests/test_configure.py
@@ -0,0 +1,59 @@
+from contextlib import contextmanager
+from pathlib import Path
+import os
+
+import pytest
+import yaml
+
+from cats.configure import config_from_file
+from cats.configure import get_location_from_config_or_args
+from cats import parse_arguments
+
+CATS_CONFIG = {
+    "location": "EH8",
+    "api": "carbonintensity.org.uk",
+}
+
+@contextmanager
+def change_dir(p):
+    current_dir = os.getcwd()
+    os.chdir(p)
+    yield
+    os.chdir(current_dir)
+
+
+@pytest.fixture
+def local_config_file(tmp_path_factory):
+    p = tmp_path_factory.mktemp("temp") / "config.yml"
+    with open(p, "w") as stream:
+        yaml.dump(CATS_CONFIG, stream)
+    return p.parent
+
+
+def test_config_from_file():
+    missing_file = "missing.yaml"
+    with pytest.raises(FileNotFoundError):
+        config_from_file(missing_file)
+        config_from_file()
+
+def test_config_from_file_default(local_config_file):
+    with change_dir(local_config_file):
+        configmapping = config_from_file()
+    assert configmapping == CATS_CONFIG
+
+def test_get_location_from_config_or_args():
+    expected_location = "SW7"
+    args = parse_arguments().parse_args(
+        ["--location", expected_location, "--duration", "1"]
+    )
+    location = get_location_from_config_or_args(args, CATS_CONFIG)
+    assert location == expected_location
+
+    args = parse_arguments().parse_args(["--duration", "1"])
+    location = get_location_from_config_or_args(args, CATS_CONFIG)
+    assert location == CATS_CONFIG["location"]
+
+    args = parse_arguments().parse_args(["--duration", "1"])
+    config = {}
+    location = get_location_from_config_or_args(args, config)
+    assert location != ""
