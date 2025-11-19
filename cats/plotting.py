e0dd94be46ae83585fa929848bd40f89c23ed440
Sadie L. Bartholomew <sadie.bartholomew@ncas.ac.uk>
2025-11-19 16:24:45 +0000

    Plotting: improve plot style esp. colours for accessibility

--- cats/plotting.py
+++ cats/plotting.py
@@ -1,10 +1,11 @@
 try:
     import matplotlib.pyplot as plt
     import matplotlib.dates as mdates
+
     have_matplotlib = True
 except ImportError:
     have_matplotlib = False
-    
+
 
 def plotplan(CI_forecast, output):
     """
@@ -14,7 +15,7 @@ def plotplan(CI_forecast, output):
         print("To plot graphs you must import matplotlib")
         print("e.g. \"pip install 'climate-aware-task-scheduler[plots]'\"")
         return None
-    
+
     # Just for now pull the CI forecast apart... probably belongs as method...
     values = []
     times = []
@@ -31,12 +32,16 @@ def plotplan(CI_forecast, output):
     for point in CI_forecast:
         values.append(point.value)
         times.append(point.datetime)
-        if (point.datetime >= output.carbonIntensityOptimal.start and
-            point.datetime <= output.carbonIntensityOptimal.end):
+        if (
+            point.datetime >= output.carbonIntensityOptimal.start
+            and point.datetime <= output.carbonIntensityOptimal.end
+        ):
             opt_values.append(point.value)
             opt_times.append(point.datetime)
-        if (point.datetime >= output.carbonIntensityNow.start and
-            point.datetime <= output.carbonIntensityNow.end):
+        if (
+            point.datetime >= output.carbonIntensityNow.start
+            and point.datetime <= output.carbonIntensityNow.end
+        ):
             now_values.append(point.value)
             now_times.append(point.datetime)
     # For our now and optimal series, end with the end data (interpolated)
@@ -46,35 +51,70 @@ def plotplan(CI_forecast, output):
     now_values.append(output.carbonIntensityNow.end_value)
 
     # Make the plot (should probably take fig and ax as opt args...)
+
+    # Use an accessibility-approved colour scheme to ensure plot is
+    # comprehendable for all (though also use hatching and clear text so
+    # that no information is only encoded by colour choice/view)
+    plt.style.use("tableau-colorblind10")
+    forecast_colour = "tab:blue"
+    now_colour = "tab:red"
+    optimal_colour = "tab:green"
+
     fig, ax = plt.subplots()
 
     # Filling under curves for the forecast, run now time and optimal run time
-    ax.fill_between(times, 0.0, values, alpha=0.2, color='b', label="Forecast")
-    # If job started now (red with hatch, black hatch lines for contrast)
     ax.fill_between(
-        now_times, 0.0, now_values,
+        times,
+        0.0,
+        values,
+        alpha=0.2,
+        color=forecast_colour,
+        edgecolor=forecast_colour,
+        label="Forecast",
+        linewidth=2.0,
+    )
+    # Show 'now' window in red with black hatch lines for contrast
+    ax.fill_between(
+        now_times,
+        0.0,
+        now_values,
         alpha=0.6,
-        color='r',
+        color=now_colour,
         label="If job started now",
         hatch="///",
-        edgecolor='k',
-        linewidth=0.0
+        edgecolor="k",
+        linewidth=1.0,
     )
-
-    # Optimal job window (green with hatch, black hatch lines for contrast)
+    # Show 'optimal' window in green with hatch lines in opposite direction
+    # but also in black: for any overlapping regions on the two windows, this
+    # therefore results in a distinguishable cross-hatch pattern
     ax.fill_between(
-        opt_times, 0.0, opt_values,
+        opt_times,
+        0.0,
+        opt_values,
         alpha=0.6,
-        color='g',
+        color=optimal_colour,
         label="Optimal job window",
         hatch="\\\\\\",
-        edgecolor='k',
-        linewidth=0.0
+        edgecolor="k",
+        linewidth=1.0,
     )
-    ax.text(0.125, 1.075, f"Mean carbon intensity if job started now: {output.carbonIntensityNow.value:.2f} gCO2eq/kWh",
-             transform=ax.transAxes, color='red')
-    ax.text(0.125, 1.025, f"Mean carbon intensity at optimal time: {output.carbonIntensityOptimal.value:.2f} gCO2eq/kWh",
-             transform=ax.transAxes, color='green')
+
+    # Add text to highlight values and for 'now' and 'optimal' job run times
+    ax.text(
+        0.125,
+        1.075,
+        f"Mean carbon intensity if job started now: {output.carbonIntensityNow.value:.2f} gCO2eq/kWh",
+        transform=ax.transAxes,
+        color=now_colour,
+    )
+    ax.text(
+        0.125,
+        1.025,
+        f"Mean carbon intensity at optimal time: {output.carbonIntensityOptimal.value:.2f} gCO2eq/kWh",
+        transform=ax.transAxes,
+        color=optimal_colour,
+    )
 
     ax.set_xlabel("Time (dd-mm-yy hh:mm)")
     ax.xaxis.set_major_formatter(mdates.DateFormatter("%d-%m-%y %H:%M"))
